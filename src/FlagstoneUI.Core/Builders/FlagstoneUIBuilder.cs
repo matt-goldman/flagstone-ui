4c10167c19ad8e1e2a970c887a5e68dfdada4c6d
Matt Goldman <matt.goldman@outlook.com>
2025-09-21 09:31:35 +1000

    Core now builds successfully, most warnings resolved

--- src/FlagstoneUI.Core/Builders/FlagstoneUIBuilder.cs
+++ src/FlagstoneUI.Core/Builders/FlagstoneUIBuilder.cs
@@ -1,6 +1,15 @@
-namespace FlagstoneUI.Core.Builders;
+﻿namespace FlagstoneUI.Core.Builders;
 
+/// <summary>
+/// Provides functionality to build and configure a Flagstone UI with customizable options.
+/// </summary>
+/// <remarks>This class supports a fluent API for configuring the UI. Call methods in a chain to apply multiple
+/// configurations.</remarks>
 public class FlagstoneUIBuilder
 {
+    /// <summary>
+    /// Configures the builder to use the default theme for the UI.
+    /// </summary>
+    /// <returns>The current <see cref="FlagstoneUIBuilder"/> instance, allowing for method chaining.</returns>
     public FlagstoneUIBuilder UseDefaultTheme() => this;
 }
--- src/FlagstoneUI.Core/Themes/ThemeLoader.cs
+++ src/FlagstoneUI.Core/Themes/ThemeLoader.cs
@@ -1,9 +1,14 @@
-using Microsoft.Maui.Controls;
+﻿namespace FlagstoneUI.Core.Themes;
 
-namespace FlagstoneUI.Core.Themes;
-
+/// <summary>
+/// Provides methods to register core Flagstone UI resource dictionaries (tokens) into a .NET MAUI application's resources.
+/// </summary>
 public static class ThemeLoader
 {
+    /// <summary>
+    /// Registers the Flagstone UI token resource dictionary into the specified application resources.
+    /// </summary>
+    /// <param name="appResources">The application's <see cref="ResourceDictionary"/> to which the tokens will be merged.</param>
     public static void Register(ResourceDictionary appResources)
     {
         appResources.MergedDictionaries.Add(new ResourceDictionary
