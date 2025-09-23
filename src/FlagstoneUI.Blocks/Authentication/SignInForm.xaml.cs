a8f77a778282c8411a6c4f443bed38d1bbe24b61
Matt Goldman <matt.goldman@outlook.com>
2025-09-24 08:57:05 +1000

    Add FsEntry styles, add basic Sign In block

---
+++ src/FlagstoneUI.Blocks/Authentication/SignInForm.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FlagstoneUI.Blocks.Authentication;
+
+public partial class SignInForm : ContentView
+{
+	public SignInForm()
+	{
+		InitializeComponent();
+	}
+
+    // TODO: Add Bindable Properties for Username, Password, SignInCommand, etc.
+}
---
+++ src/FlagstoneUI.Blocks/GlobalXmlns.cs
@@ -0,0 +1,3 @@
+﻿using Microsoft.Maui.Controls.Xaml;
+
+[assembly: XmlnsDefinition("http://flagstoneui.com/schemas", "FlagstoneUI.Blocks.Authentication")]
