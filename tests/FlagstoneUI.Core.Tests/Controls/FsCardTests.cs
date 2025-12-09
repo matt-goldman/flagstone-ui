7daa89dcdc6cabf7147753c918b184841f7d075e
Matt Goldman <matt.goldman@outlook.com>
2025-12-09 19:41:35 +1100

    refactor: simplify FsCardTests by using object initializer for property assignments

--- tests/FlagstoneUI.Core.Tests/Controls/FsCardTests.cs
+++ tests/FlagstoneUI.Core.Tests/Controls/FsCardTests.cs
@@ -23,33 +23,29 @@ public class FsCardTests : MauiTestBase
 	[Fact]
 	public void Card_elevation_property_can_be_set()
 	{
-		var card = new FsCard();
-		card.Elevation = 2;
+		var card = new FsCard { Elevation = 2 };
 		card.Elevation.ShouldBe(2);
 	}
 
 	[Fact]
 	public void Card_with_zero_elevation_has_no_shadow()
 	{
-		var card = new FsCard();
-		card.Elevation = 0;
+		var card = new FsCard { Elevation = 0 };
 		card.Shadow.ShouldBeNull();
 	}
 
 	[Fact]
 	public void Card_with_positive_elevation_has_shadow()
 	{
-		var card = new FsCard();
-		card.Elevation = 2;
+		var card = new FsCard { Elevation = 2 };
 		card.Shadow.ShouldNotBeNull();
 	}
 
 	[Fact]
 	public void Card_shadow_radius_increases_with_elevation()
 	{
-		var card = new FsCard();
+		var card = new FsCard { Elevation = 1 };
 
-		card.Elevation = 1;
 		var shadow1Radius = card.Shadow?.Radius ?? 0f;
 
 		card.Elevation = 3;
@@ -61,32 +57,28 @@ public class FsCardTests : MauiTestBase
 	[Fact]
 	public void Card_corner_radius_property_can_be_set()
 	{
-		var card = new FsCard();
-		card.CornerRadius = 12.0;
+		var card = new FsCard { CornerRadius = 12.0 };
 		card.CornerRadius.ShouldBe(12.0);
 	}
 
 	[Fact]
 	public void Card_border_color_property_can_be_set()
 	{
-		var card = new FsCard();
-		card.BorderColor = Colors.Red;
+		var card = new FsCard { BorderColor = Colors.Red };
 		card.BorderColor.ShouldBe(Colors.Red);
 	}
 
 	[Fact]
 	public void Card_border_width_property_can_be_set()
 	{
-		var card = new FsCard();
-		card.BorderWidth = 2.0;
+		var card = new FsCard { BorderWidth = 2.0 };
 		card.BorderWidth.ShouldBe(2.0);
 	}
 
 	[Fact]
 	public void Card_background_color_property_can_be_set()
 	{
-		var card = new FsCard();
-		card.BackgroundColor = Colors.Blue;
+		var card = new FsCard { BackgroundColor = Colors.Blue };
 		card.BackgroundColor.ShouldBe(Colors.Blue);
 	}
 }
