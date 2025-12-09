c978fd4891ebd6e5b9a46abce621615321ad0501
Matt Goldman <matt.goldman@outlook.com>
2025-12-10 08:10:41 +1100

    Restored control showcase page and added FsEditor to themes

--- samples/FlagstoneUI.SampleApp/App.xaml.cs
+++ samples/FlagstoneUI.SampleApp/App.xaml.cs
@@ -12,7 +12,7 @@ public partial class App : Application
 
 	protected override Window CreateWindow(IActivationState? activationState)
 	{
-		return new Window(new MainPage());
+		return new Window(new AppShell());
 	}
 
 	public static void SwitchTheme(string theme)
