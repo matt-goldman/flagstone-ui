cf9d350df551d032c22baed7a81a577546d3579e
Matt Goldman <matt.goldman@outlook.com>
2025-12-10 15:31:02 +1100

    Added validation demo to sample app and removed MCT integration test project

--- samples/MctIntegrationsTest/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MctIntegrationsTest;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- samples/MctIntegrationsTest/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MctIntegrationsTest;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
