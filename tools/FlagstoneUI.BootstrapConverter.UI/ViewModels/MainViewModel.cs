fe0c65e4cafa5250776e40ca288bbc1474718866
Matt Goldman <matt.goldman@outlook.com>
2025-12-14 08:15:27 +1100

    Updated UI for BootstrapConverter

--- tools/FlagstoneUI.BootstrapConverter.UI/ViewModels/MainViewModel.cs
+++ tools/FlagstoneUI.BootstrapConverter.UI/ViewModels/MainViewModel.cs
@@ -8,7 +8,53 @@ namespace FlagstoneUI.BootstrapConverter.UI.ViewModels;
 
 public partial class MainViewModel(IFileService file, IBootstrapService bootstrapService) : ObservableObject
 {
-	public ObservableCollection<string> SelectedFiles { get; set; } = [];
+	public ObservableCollection<ResourceDictionaryFormat> ResourceDictionaryFormats { get; set; } = 
+	[
+		ResourceDictionaryFormat.CSharp,
+		ResourceDictionaryFormat.Xaml,
+	];
+
+	[ObservableProperty]
+	public partial ResourceDictionaryFormat OutputType { get; set; }
+
+
+	public ObservableCollection<DarkModeStrategy> DarkModeStrategies { get; set; } =
+	[
+			DarkModeStrategy.Auto,
+			DarkModeStrategy.Manual,
+		DarkModeStrategy.None
+
+	];
+
+	[ObservableProperty]
+	public partial DarkModeStrategy DarkModeStrategy { get; set; }
+
+	
+	public ObservableCollection<AnalysisStrategy> AnalysisStrategies { get; set; } =
+	[
+		AnalysisStrategy.CssOnly,
+		AnalysisStrategy.Hybrid,
+		AnalysisStrategy.VariablesOnly
+	];
+
+	[ObservableProperty]
+	public partial AnalysisStrategy AnalysisStrategy { get; set; }
+
+
+	public ObservableCollection<BootstrapFormat> BootstrapFormats { get; set; } =
+	[
+		BootstrapFormat.Auto,
+		BootstrapFormat.Css,
+		BootstrapFormat.Scss
+	];
+
+	[ObservableProperty]
+	public partial BootstrapFormat BootstrapFormat { get; set; }
+
+
+
+	[ObservableProperty]
+	public partial string ConversionResults { get; set; }
 
 	[ObservableProperty]
 	public partial string ThemeName { get; set; }
@@ -19,21 +65,6 @@ public partial class MainViewModel(IFileService file, IBootstrapService bootstra
 	[ObservableProperty]
 	public partial bool IncludeFonts { get; set; }
 
-	[ObservableProperty]
-	public partial ResourceDictionaryFormat OutputType { get; set; }
-
-	[ObservableProperty]
-	public partial DarkModeStrategy DarkModeStrategy { get; set; }
-
-	[ObservableProperty]
-	public partial AnalysisStrategy AnalyisStrategy { get; set; }
-
-	[ObservableProperty]
-	public partial BootstrapFormat BootstrapFormat { get; set; }
-
-	[ObservableProperty]
-	public partial string ConversionResults { get; set; }
-
-
+	public ObservableCollection<string> SelectedFiles { get; set; } = [];
 
 }
