775dfdba171be0dd95239970197b8f91aa933df0
Pedro Jesus <pedrojesus.cefet@gmail.com>
2025-12-14 17:41:11 -0300

    Fix Handler registration (#53)
    * Adjust the RegisterHandler to be called only once
    
    * removed unused code
    
    ---------
    
    Co-authored-by: Pedro Jesus <pedrojesus@Pedros-MacBook-Pro.local>

--- src/FlagstoneUI.Core/Platforms/Windows/Controls/BorderlessEditor.cs
+++ src/FlagstoneUI.Core/Platforms/Windows/Controls/BorderlessEditor.cs
@@ -2,18 +2,18 @@
 
 public partial class BorderlessEditor
 {
-	partial void RegisterHandler()
+	internal static partial void RegisterHandler()
 	{
 		Microsoft.Maui.Handlers.EditorHandler.Mapper.AppendToMapping("Borderless", (handler, view) =>
 		{
 			if (view is BorderlessEditor)
 			{
 				handler.PlatformView.BorderThickness = new Microsoft.UI.Xaml.Thickness(0);
-                handler.PlatformView.Background = null;
-                handler.PlatformView.FocusVisualMargin = new Microsoft.UI.Xaml.Thickness(0);
-                
-                // Disable all focus visuals (which may trigger on hover too)
-                handler.PlatformView.UseSystemFocusVisuals = false;
+				handler.PlatformView.Background = null;
+				handler.PlatformView.FocusVisualMargin = new Microsoft.UI.Xaml.Thickness(0);
+
+				// Disable all focus visuals (which may trigger on hover too)
+				handler.PlatformView.UseSystemFocusVisuals = false;
 			}
 		});
 	}
